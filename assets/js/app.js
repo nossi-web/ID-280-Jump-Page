64feb756ca059a293a673ba3ac101d3c7e1cd5e3
belmont-web <joel.norris@adjuncts.belmont.edu>
2020-05-26 14:39:28 -0500

    added app.js, added layout markup, header, main, footer, nav, added motd script

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+/* app.js */
+
+// create a variable motd and assign a string to it
+let motd = "Have a goodun, bud!";
+
+// query the document and get an element with an id of 'motd'
+// and assign our character string to it.
+document.querySelector("#motd").innerHTML = motd;
