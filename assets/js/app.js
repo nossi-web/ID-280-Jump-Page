f3b76a1e7df71ead56d48aa19c9902a8bf960c3c
belmont-web <joel.norris@adjuncts.belmont.edu>
2020-06-02 15:30:23 -0500

    added random MOTD generator

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,27 @@
 /* app.js */
 
 // create a variable motd and assign a string to it
-let motd = "Have a goodun, bud!";
+let motdArray = [
+    "You miss 100% of the shots you don't take <i>--Wayne Gretsky</i>",
+    "Everything you can imagine is real <i>--Pablo Picasso</i>",
+    "Unless someone like you cares a whole awful lot, nothing is going to get better, it's not. <i>--Dr. Seuss</i>",
+    "No matter what people tell you, words and ideas can change the world. <i>--Robin Williams</i>",
+    "Be yourself, everyone else is already taken. <i>--Oscar Wilde</i>",
+    "Find something you love to do and then do it for the rest of your life <i>--Max Fischer</i>"
+
+];
+
+
+// getRandomInt function procured from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+let randomIndex = getRandomInt(motdArray.length);
+
+console.log("randomIndex is " + randomIndex);
 
 // query the document and get an element with an id of 'motd'
 // and assign our character string to it.
-document.querySelector("#motd").innerHTML = motd;
+document.querySelector("#motd").innerHTML = motdArray[randomIndex];
